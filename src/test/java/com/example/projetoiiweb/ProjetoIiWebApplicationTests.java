82e80285a803f40124b5a72a40293011540b547c
lopesnuno <lopesnuno@ipvc.pt>
2023-07-09 10:06:12 +0100

    Initial application structure

---
+++ src/test/java/com/example/projetoiiweb/ProjetoIiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoiiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
