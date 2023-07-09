9b961c184c02d2a5ebc8a010c3e90d3f27e93ed1
lopesnuno <lopesnuno@ipvc.pt>
2023-07-09 12:33:34 +0100

    Login flow

--- src/main/java/com/example/projetoiiweb/controllers/AuthController.java
+++ src/main/java/com/example/projetoiiweb/controllers/AuthController.java
@@ -2,15 +2,18 @@ package com.example.projetoiiweb.controllers;
 
 import com.example.projetoiiweb.models.Cliente;
 import com.example.projetoiiweb.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Optional;
 
 @Controller
+@SessionAttributes({"isLoggedIn", "user"})
 public class AuthController {
     private final UserRepository userRepository;
 
@@ -19,18 +22,21 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String loginPage(Model model) {
+    public String loginPage(Model model){
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model){
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model, HttpSession session){
         Optional<Cliente> user = userRepository.findUserByUsername(username);
         if (user.isPresent()){
             Cliente currentUser = user.get();
             System.out.println(currentUser);
-            if (currentUser.getUsername().equals(username) && currentUser.getPassword().equals(password)){
+            if (currentUser.getUsername().equals(username) && currentUser.getPassword().equals(password)) {
+                session.setAttribute("isLoggedIn", true);
                 model.addAttribute("isLoggedIn", true);
+                model.addAttribute("user", currentUser);
+
                 return "redirect:/atividades";
             }
             return "redirect:/error";
---
+++ src/main/java/com/example/projetoiiweb/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.projetoiiweb.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(HttpSession session, Model model) {
+        Boolean isLoggedIn = session.getAttribute("isLoggedIn") != null && (boolean) session.getAttribute("isLoggedIn");
+        if (isLoggedIn) {
+            model.addAttribute("isLoggedIn", isLoggedIn);
+            return "index";
+        }
+        return "login";
+    }
+}
